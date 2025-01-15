f3df078a8465005a29afb301715dc4725ed970b8
paulina <paulinashypulia@gmail.com>
2025-01-15 14:06:45 +0400

    changed_method_names

--- tests/test_eco_friendly_page_options.py
+++ tests/test_eco_friendly_page_options.py
@@ -5,10 +5,10 @@ def test_default_pagination_option(eco_friendly_page):
 
 def test_filter_by_size(eco_friendly_page):
     eco_friendly_page.open_page()
-    eco_friendly_page.select_size()
+    eco_friendly_page.select_xs_size_and_check_items_labels()
     eco_friendly_page.clear_filter()
 
 
 def test_filter_by_price(eco_friendly_page):
     eco_friendly_page.open_page()
-    eco_friendly_page.filter_price()
+    eco_friendly_page.filter_price_and_check_items_price_is_in_range()
